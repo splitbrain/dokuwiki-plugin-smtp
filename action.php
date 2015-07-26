6ab179d20eb5805745647ce412329c70cd97c74b
Andreas Gohr <andi@splitbrain.org>
2015-07-26 15:47:25 +0200

    actually use config vars and show debug data

--- action.php
+++ action.php
@@ -35,23 +35,58 @@ class action_plugin_smtp extends DokuWiki_Action_Plugin {
     public function handle_mail_message_send(Doku_Event &$event, $param) {
         require_once __DIR__ . '/loader.php';
 
+        // prepare the message
         /** @var Mailer $mailer Our Mailer with all the data */
         $mailer = $event->data['mail'];
         $rcpt   = $mailer->cleanAddress($event->data['to']) . ',' .
                   $mailer->cleanAddress($event->data['cc']) . ',' .
                   $mailer->cleanAddress($event->data['bcc']);
         $from   = $event->data['from'];
-
         $message = new \splitbrain\dokuwiki\plugin\smtp\Message(
             $from,
             $rcpt,
             $mailer->dump()
         );
 
-        $smtp = new \Tx\Mailer\SMTP();
-        $smtp->setServer('localhost', 2525, null);
+        // prepare the SMTP communication lib
+        $logger = new \splitbrain\dokuwiki\plugin\smtp\Logger();
+        $smtp = new \Tx\Mailer\SMTP($logger);
+        $smtp->setServer(
+            $this->getConf('smtp_host'),
+            $this->getConf('smtp_port'),
+            $this->getConf('smtp_ssl')
+        );
+        if($this->getConf('auth_user')){
+            $smtp->setAuth(
+                $this->getConf('auth_user'),
+                $this->getConf('auth_pass')
+            );
+        }
 
-        $smtp->send($message);
+        // send the message
+        try {
+            $smtp->send($message);
+            $ok = true;
+        } catch (Exception $e) {
+            msg('There was an unexpected problem communicating with SMTP: '.$e->getMessage(), -1);
+            $ok = false;
+        }
+
+        // give debugging help on error
+        if(!$ok && $this->getConf('debug')) {
+            $log = array();
+            foreach($logger->getLog() as $line) {
+                $log[] = $line[1];
+            }
+            $log = join("\n", $log);
+            msg('SwiftMailer log:<br /><pre>'.hsc($log).'</pre>',-1);
+        }
+
+        // finish event handling
+        $event->preventDefault();
+        $event->stopPropagation();
+        $event->result = $ok;
+        $event->data['success'] = $ok;
     }
 
 }
