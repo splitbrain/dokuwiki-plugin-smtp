3ff87f82375b64a660aa9ef556db2c27b8cf176a
Andreas Gohr <andi@splitbrain.org>
2015-07-26 17:49:36 +0200

    added testing mechanism as in SwiftMail

--- admin.php
+++ admin.php
@@ -1,50 +1,71 @@
 <?php
 /**
- * DokuWiki Plugin smtp (Admin Component)
+ * Swiftmail Plugin
  *
- * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
- * @author  Andreas Gohr <andi@splitbrain.org>
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Andreas Gohr <andi@splitbrain.org>
  */
-
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 
 class admin_plugin_smtp extends DokuWiki_Admin_Plugin {
 
     /**
-     * @return int sort number in admin menu
+     * return sort order for position in admin menu
      */
-    public function getMenuSort() {
-        return 500;
+    function getMenuSort() {
+        return 200;
     }
 
     /**
-     * @return bool true if only access for superuser, false is for superusers and moderators
+     * handle user request
      */
-    public function forAdminOnly() {
-        return false;
+    function handle() {
+        global $INPUT;
+        global $conf;
+        if(!$INPUT->bool('send')) return;
+
+        // make sure debugging is on;
+        $conf['plugin']['smtp']['debug'] = 1;
+
+        // send a mail
+        $mail = new Mailer();
+        if($INPUT->str('to')) $mail->to($INPUT->str('to'));
+        if($INPUT->str('cc')) $mail->to($INPUT->str('cc'));
+        if($INPUT->str('bcc')) $mail->to($INPUT->str('bcc'));
+        $mail->subject('DokuWiki says hello');
+        $mail->setBody("Hi @USER@\n\nThis is a test from @DOKUWIKIURL@");
+        $ok = $mail->send();
+
+        // check result
+        if($ok){
+            msg('Message was sent. SMTP seems to work.',1);
+        }else{
+            msg('Message wasn\'t sent. SMTP seems not to work properly.',-1);
+        }
     }
 
     /**
-     * Should carry out any processing required by the plugin.
+     * Output HTML form
      */
-    public function handle() {
+    function html() {
+        global $INPUT;
+        global $conf;
+
+        echo $this->locale_xhtml('intro');
+
+        if(!$conf['mailfrom']) msg($this->getLang('nofrom'),-1);
+
+
+        $form = new Doku_Form(array());
+        $form->startFieldset('Testmail');
+        $form->addHidden('send', 1);
+        $form->addElement(form_makeField('text', 'to', $INPUT->str('to'), 'To:', '', 'block'));
+        $form->addElement(form_makeField('text', 'cc', $INPUT->str('cc'), 'Cc:', '', 'block'));
+        $form->addElement(form_makeField('text', 'bcc', $INPUT->str('bcc'), 'Bcc:', '', 'block'));
+        $form->addElement(form_makeButton('submit', '', 'Send Email'));
+
+        $form->printForm();
     }
 
-    /**
-     * Render HTML output, e.g. helpful text and a form
-     */
-    public function html() {
-        ptln('<h1>'.$this->getLang('menu').'</h1>');
-
-
-        require_once __DIR__ . '/loader.php';
-
-
-        $logger = new \splitbrain\dokuwiki\plugin\smtp\Logger();
-        $mailer = new Tx\Mailer\SMTP($logger);
-
-    }
 }
-
-// vim:ts=4:sw=4:et:
