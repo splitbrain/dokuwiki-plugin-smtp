32eb8bd1fbc14327ee44023f0497edeb24a668bd
hideaki SAWADA <chuno@live.jp>
2015-08-11 20:30:38 +0200

    translation update

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -2,16 +2,17 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
+ * 
  * @author Hideaki SAWADA <chuno@live.jp>
+ * @author hideaki SAWADA <chuno@live.jp>
  */
-$lang['smtp_host']             = '送信SMTPサーバー';
-$lang['smtp_port']             = 'SMTPサーバーのポート。通常は 25。SSL の場合は 465。';
-$lang['smtp_ssl']              = 'SMTPサーバーとの通信する時、使用される暗号化は？';
-$lang['smtp_ssl_o_']          = 'なし';
-$lang['smtp_ssl_o_ssl']          = 'SSL';
-$lang['smtp_ssl_o_tls']          = 'TLS';
+$lang['smtp_host']             = '送信 SMTP サーバー';
+$lang['smtp_port']             = 'SMTP サーバーのポート。通常は 25。SSL の場合は 465。';
+$lang['smtp_ssl']              = 'SMTP サーバーと通信する時、使用される暗号化は？';
+$lang['smtp_ssl_o_']           = 'なし';
+$lang['smtp_ssl_o_ssl']        = 'SSL';
+$lang['smtp_ssl_o_tls']        = 'TLS';
 $lang['auth_user']             = '認証が必要な場合のユーザー名';
 $lang['auth_pass']             = '上記ユーザーのパスワード';
-$lang['localdomain']           = 'MTP HELOのフェーズ中に使用される名前。DokuWiki が稼働している Webサーバーの FQDN です。自動検出させる場合、空のままにします。';
-$lang['debug']                 = '送信が失敗した場合、完全なエラーログを印刷？全てが稼働する場合、無効にして下さい！';
+$lang['localdomain']           = 'SMTP の HELO フェーズ中に使用される名前。DokuWiki が稼働している Web サーバーの FQDN です。自動検出させる場合、空のままにします。';
+$lang['debug']                 = '送信が失敗した場合、完全なエラーログを印刷しますか？稼働確認後、無効にして下さい！';
