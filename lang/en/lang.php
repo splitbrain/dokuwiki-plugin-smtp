4e7bb93e854c76222c0cedd8b087d7b493abac08
blacklord049 <vincent.lecomte@outlook.be>
2016-03-23 16:27:25 +0100

    Language strings in action+admin.php files

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -2,3 +2,14 @@
 
 $lang['menu'] = 'Check SMTP configuration';
 $lang['nofrom'] = 'You did not configure the \'mailfrom\' option. Sending mails will probably fail.';
+
+// Page administration.
+$lang['testtitle']	= 'Test mail';
+$lang['sentok']     = 'Message was sent. SMTP seems to work.';
+$lang['sentnok']    = 'Message wasn\'t sent. SMTP seems not to work properly.';
+$lang['to']    		= 'To:';
+$lang['sendemail']	= 'Send mail';
+
+// Page action.
+$lang['unexpectedsmtp']	= 'There was an unexpected problem communicating with SMTP: ';
+$lang['abovemaywarn']	= 'Above may contain passwords - do not post online!';
