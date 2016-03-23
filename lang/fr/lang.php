63fafcc2bc77cdaefb84d7d77373631ca40845dd
blacklord049 <vincent.lecomte@outlook.be>
2016-03-23 16:28:00 +0100

    French strings in action+admin.php files

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -5,6 +5,18 @@
  *
  * @author Schplurtz le Dévoulonné <Schplurtz@laposte.net>
  * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
+ * Mis à jour par Vincent L le 23/3/2016.
  */
-$lang['menu']                  = 'Vérifier la configuration de SMTP';
-$lang['nofrom']                = 'Vous n\'avez pas configuré l\'option \'De\'. L\'envoi de courriel va probablement échouer.';
+$lang['menu']       = 'Vérifier la configuration de SMTP';
+$lang['nofrom']     = 'Vous n\'avez pas configuré l\'option \'De\'. L\'envoi de courriel va probablement échouer.';
+
+// Page administration.
+$lang['testtitle']	= 'Tester l\'envoi de mails';
+$lang['sentok']     = 'Le message a bien été envoyé.';
+$lang['sentnok']    = 'Le message n\'a pas été envoyé. Il peut y avoir un problème au niveau de la configuration SMTP.';
+$lang['to']    		= 'A:';
+$lang['sendemail']	= 'Envoyer';
+
+// Page action.
+$lang['unexpectedsmtp']	= 'Une erreur SMTP inattendue s\'est produite lors de l\'envoi : ';
+$lang['abovemaywarn']	= 'Le log ci-dessus peut contenir des mots de passe. Veillez à ne pas les communiquer.';
