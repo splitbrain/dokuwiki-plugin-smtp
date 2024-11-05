2dd212db3b1aa24357a5de59242305d62371218a
admin baoz <admin@baoz.vn>
2024-11-05 12:50:46 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,4 +1,9 @@
 <?php
 
-$lang['menu'] = 'Check SMTP configuration';
-$lang['nofrom'] = 'You did not configure the \'mailfrom\' option. Sending mails will probably fail.';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author admin baoz <admin@baoz.vn>
+ */
+$lang['menu']                  = 'Kiểm tra cấu hình SMTP';
+$lang['nofrom']                = 'Bạn đã không cấu hình tuỳ chọn \'thư tới từ\'. Việc gửi thư sẽ có thể thất bại';
