60e3e1c88889efe7d08ea44a4453b6db9c915019
Wizzard <wizzardsk@gmail.com>
2026-06-04 20:06:33 +0200

    Translation update (sk)

---
+++ lang/sk/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['menu']                  = 'Kontrola nastavenia SMTP';
+$lang['nofrom']                = 'Nemáte nastavenú voľbu \'mailfrom\'. Odosielanie e-mailov pravdepodobne zlyhá.';
